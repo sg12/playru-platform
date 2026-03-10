79f20dbc340ef057f098d7d4bf7269797ac40401
Sergey <sg12@ngs.ru>
2026-03-10 15:58:39 +0700

    no message

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('', include('apps.store.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
